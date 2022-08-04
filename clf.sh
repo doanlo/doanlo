81491bb01ff35fbfd787fc839e5c8474a5654c3e
doanlo <110599547+doanlo@users.noreply.github.com>
2022-08-04 16:27:11 +0700

    Update clf.sh

--- clf.sh
+++ clf.sh
@@ -5,25 +5,24 @@ echo "1. Check ID ACC"
 echo "2. Check Zone ID && WEB"
 echo "3. DELETE WEB"
 read -p "  Vui Lòng Nhập : " num
-
-    case "${num}" in
+case "${num}" in
         1) curl -X GET "https://api.cloudflare.com/client/v4/accounts?page=1&per_page=20&direction=desc" \
                 -H "X-Auth-Email: $user" \
                 -H "X-Auth-Key: $api" \
                 -H "Content-Type: application/json"
         ;;
         2) read -p " ID ACC : " id
-         curl -X GET "https://api.cloudflare.com/client/v4/zones?status=active&account.id=$id&page=1&per_page=20&order=status&direction=desc" \
+           curl -X GET "https://api.cloudflare.com/client/v4/zones?status=active&account.id=$id&page=1&per_page=20&order=status&direction=desc" \
                 -H "X-Auth-Email: $user" \
                 -H "X-Auth-Key: $api" \
                 -H "Content-Type: application/json"
         ;;
         3) read -p " ZONE ID : " zoneid
-         curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$zoneid" \
+           curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$zoneid" \
                 -H "X-Auth-Email: $user" \
                 -H "X-Auth-Key: $api" \
                 -H "Content-Type: application/json"
         ;;
         *) rm -f $HISTFILE && unset HISTFILE && exit
         ;;
-    esac
+esac
